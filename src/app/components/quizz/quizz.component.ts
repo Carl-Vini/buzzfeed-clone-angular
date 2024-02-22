b8f63beb1a3562098f12bd606d7785b3ac144f94
vitor Higor <carlosviniciussampaio@hotmail.com>
2024-02-21 22:25:50 -0300

    adicionando os assets

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+
+}
